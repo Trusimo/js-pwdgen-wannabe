fc18c8006c0c823a8c59004a91d98399eaa91a55
Trusimo <simonetrunfio96@gmail.com>
2022-05-17 16:47:30 +0200

    adds "" in getdocumentbyid

--- main.js
+++ main.js
@@ -2,6 +2,4 @@ var nomeUtente = prompt("Qual è il tuo nome?");
 var cognomeUtente = prompt("Qual è il tuo cognome?");
 var colorePreferitoUtente = prompt("Qual è il tuo colore preferito?");
 
-document.getElementById(NomeCognomeColore).innerHTML = (nomeUtente + cognomeUtente + colorePreferitoUtente);
-
-console.log (nomeUtente + cognomeUtente + colorePreferitoUtente)
+document.getElementById("NomeCognomeColore").innerHTML = (nomeUtente + cognomeUtente + colorePreferitoUtente);
