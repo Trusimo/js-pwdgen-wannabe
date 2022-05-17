d3268585df88bdeefc760fa27b05c969d7fb4aba
Trusimo <simonetrunfio96@gmail.com>
2022-05-17 16:14:44 +0200

    creating prompt

---
+++ main.js
@@ -0,0 +1,7 @@
+const nomeUtente = prompt("Qual è il tuo nome?");
+const cognomeUtente = prompt("Qual è il tuo cognome?");
+const colorePreferitoUtente = prompt("Qual è il tuo colore preferito?");
+
+document.getElementById(NomeCognomeColore).innerHTML = (nomeUtente + cognomeUtente + colorePreferitoUtente);
+
+console.log (nomeUtente + cognomeUtente + colorePreferitoUtente)
